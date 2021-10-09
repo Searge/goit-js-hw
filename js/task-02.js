2ddd763ae8b5e42421f79eac7bd1286c47e417b5
Searge <ashikcygan@gmail.com>
2021-10-09 14:41:38 +0300

    hw-06; task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngrid = document.querySelector('#ingredients');
+
+ingredients.forEach(ingridient => {
+  const liIngrid = document.createElement('li');
+
+  liIngrid.textContent = ingridient;
+  ulIngrid.appendChild(liIngrid);
+});
