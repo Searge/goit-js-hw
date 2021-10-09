ce2a5e14a05d45727abe78a7990d6033fb09285a
Searge <ashikcygan@gmail.com>
2021-10-09 16:05:24 +0300

    hw-06; task 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output')
+
+const inputName = () => {
+    spanEl.textContent = inputEl.value
+    if (inputEl.value === "") {
+        spanEl.textContent = 'Anonymous'
+    }
+}
+
+inputEl.addEventListener('blur', inputName)
